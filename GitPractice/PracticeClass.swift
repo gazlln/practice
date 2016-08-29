169647e2fd0f345ab9e4d2b89aa18e1af7aeeade
gazlln <gazlong@me.com>
2016-08-29 21:20:34 +0100

    added test1

---
+++ GitPractice/PracticeClass.swift
@@ -0,0 +1,16 @@
+//
+//  PracticeClass.swift
+//  GitPractice
+//
+//  Created by Gaz Long on 29/08/2016.
+//  Copyright © 2016 Gaz Long. All rights reserved.
+//
+
+import Foundation
+
+class hello {
+    
+    var test1:Int!
+    
+    
+}
